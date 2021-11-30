f8486e3dea82fc64d5df28bb923c00abc3b7db99
Harikrishnan <harikrishnan19081991@gmail.com>
2021-11-30 10:00:33 +0530

    create basic sidebar text

---
+++ components/Sidebar.js
@@ -0,0 +1,9 @@
+function SideBar() {
+  return (
+    <div>
+      <h1>I am a sidebar</h1>
+    </div>
+  );
+}
+
+export default SideBar;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-import Head from "next/head";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Spotify Clone</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>This is a dope spotify clone 2.0</h1>
+    <div className="">
       <main>
         {/* Sidebar */}
+        <Sidebar />
         {/* center */}
       </main>
       <div>{/* Player */}</div>

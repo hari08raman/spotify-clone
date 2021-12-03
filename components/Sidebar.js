366143aac718cd07e76d7136622558318244df16
Harikrishnan <harikrishnan19081991@gmail.com>
2021-12-03 22:12:02 +0530

    add second level icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,6 +3,8 @@ import {
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
+  HeartIcon,
+  RssIcon,
 } from "@heroicons/react/outline";
 
 function SideBar() {
@@ -20,6 +22,20 @@ function SideBar() {
         <button className="flex items-center space-x-2 hover:text-white">
           <LibraryIcon className="h-5 w-5" />
           <p>Your Library</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-900" />
+
+        <button className="flex items-center space-x-2 hover:text-white">
+          <PlusCircleIcon className="h-5 w-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5" />
+          <p>Your Episodes</p>
         </button>
         <hr className="border-t-[0.1px] border-gray-900" />
       </div>

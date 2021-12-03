24111664e2f41d54deac4f2b959a8ebe7f37a4aa
Harikrishnan <harikrishnan19081991@gmail.com>
2021-12-03 22:23:56 +0530

    add background color and spacing

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-black h-screen overflow-hidden">
       <main>
         {/* Sidebar */}
         <Sidebar />
